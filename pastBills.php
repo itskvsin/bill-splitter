a4d336645dc77e5e2dc5ff9b42fe4aa68cd6889f
Kevin <kevinsolankistudy@gmail.com>
2025-04-20 19:31:13 +0530

    files for showing past bill is created

---
+++ pastBills.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/pastBills.css">
+    <title>Past Bills</title>
+</head>
+<body>
+    This is past Bills Page
+</body>
+</html>
