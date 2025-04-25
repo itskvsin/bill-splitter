226dea5bdf489be7aebf0572bbcd2abab5bc575d
Kevin <kevinsolankistudy@gmail.com>
2025-04-25 22:59:28 +0530

    made canges for the hosting in infinityFree

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db = 'bill-splitter';
+$host = 'sql203.infinityfree.com';
+$user = 'if0_38746285';
+$pass = '9265340766';
+$db = 'if0_38746285_billsplitter';
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
