99d574bd4893c72912edf21e4ca59e51f66c1493
Kevin <kevinsolankistudy@gmail.com>
2025-04-25 23:40:11 +0530

    backend for fetching past bills data completed and connection configuration changed to local hosting

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = 'sql203.infinityfree.com';
-$user = 'if0_38746285';
-$pass = '9265340766';
-$db = 'if0_38746285_billsplitter';
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'bill-splitter';
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
--- storedPastBills.php
+++ storedPastBills.php
@@ -1,3 +1,29 @@
 <?php 
-    
+    include 'config.php';
+
+    $bills = [];
+
+    $result = $conn->query("SELECT * FROM bill ORDER BY created_at DESC");
+
+    while($row = $result->fetch_assoc()){
+        $billId = $row['id'];
+        $participants = [];
+
+        $stmt = $conn->prepare("SELECT * FROM participants WHERE bill_id = ?");
+        $stmt->bind_param('i' , $billId);
+        $stmt->execute();
+        $participantsResult = $stmt->get_result();
+
+        while($p = $participantsResult->fetch_assoc()){
+            $participants[] = $p;
+        }
+
+        $bills[] = [
+            'title' => $row['title'],
+            'paid_by' => $row['paid_by'],
+            'amount' => $row['total_amt'],
+            'created_at' => $row['created_at'],
+            'participants' => $participants
+        ];
+    }
 ?>
