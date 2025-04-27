16495ee02257e9d8b3ce89d7391a9e899a9aa47a
Kevin <kevinsolankistudy@gmail.com>
2025-04-27 13:16:35 +0530

    updated the participants section

--- JS/extraPersons.js
+++ JS/extraPersons.js
@@ -1,34 +1,40 @@
 document.getElementById('generatePersons').addEventListener('click', function(e) {
     const num = document.getElementById('personNum').value;
     const container = document.querySelector('.persons');
-    container.innerHTML = ''; 
+    container.innerHTML = '';
 
     for (let i = 0; i < num; i++) {
         const personDiv = document.createElement('div');
         personDiv.classList.add('personEntry');
 
-        const personName = document.createElement('input');
-        personName.type = 'text';
-        personName.name = `personName${i + 1}`;
-        personName.placeholder = `Enter the name:`;
-        personName.classList.add = 'personName';
+        const nameInput = document.createElement('input');
+        nameInput.type = 'text';
+        nameInput.name = `personName${i + 1}`;
+        nameInput.placeholder = 'Enter the name';
+        nameInput.classList.add('personName');
+
+        const bottomRow = document.createElement('div');
+        bottomRow.classList.add('bottomRow');
 
         const contactInput = document.createElement('input');
         contactInput.type = 'text';
         contactInput.name = `personContact${i + 1}`;
         contactInput.placeholder = `Email or Phone for person ${i + 1}`;
-        contactInput.classList.add = 'personContact';
+        contactInput.classList.add('personContact');
 
         const amountInput = document.createElement('input');
         amountInput.type = 'number';
         amountInput.step = '0.01';
         amountInput.name = `personAmount${i + 1}`;
-        amountInput.placeholder = `₹ Amount`;
-        amountInput.classList.add = 'personAmount'
+        amountInput.placeholder = '₹ Amount';
+        amountInput.classList.add('personAmount');
 
-        personDiv.appendChild(personName)
-        personDiv.appendChild(contactInput);
-        personDiv.appendChild(amountInput);
+        bottomRow.appendChild(contactInput);
+        bottomRow.appendChild(amountInput);
+
+        personDiv.appendChild(nameInput);
+        personDiv.appendChild(bottomRow);
+
         container.appendChild(personDiv);
     }
 });
